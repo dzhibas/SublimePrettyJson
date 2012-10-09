d529976ca0133fd9286a37ba986d999477944e38
Nikolajus <nikolajus@gmail.com>
2012-10-09 15:57:06 +0200

    adding ensure_ascii to configuration, and renaming configuration variable indent_size to indent. it's possible to specify not only the size of spaces but also string

--- PrettyJson.py
+++ PrettyJson.py
@@ -27,6 +27,6 @@ class PrettyjsonCommand(sublime_plugin.TextCommand):
 
             try:
                 obj = json.loads(self.view.substr(selection), object_pairs_hook=OrderedDict)
-                self.view.replace(edit, selection, json.dumps(obj, indent=s.get("indent_size", 4), ensure_ascii=False, sort_keys=s.get("sort_keys", False), separators=(',', ': ')))
+                self.view.replace(edit, selection, json.dumps(obj, indent=s.get("indent", 4), ensure_ascii=s.get("ensure_ascii", False), sort_keys=s.get("sort_keys", False), separators=(',', ': ')))
             except Exception, e:
                 sublime.status_message(str(e))

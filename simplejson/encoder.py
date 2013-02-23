c4d1a36095bf1af58bfcf82a0a10b3089eee842d
Chris Castle <crcastle@gmail.com>
2013-02-22 23:11:36 -0800

    Fixes import error in simplejson I mistakenly added

--- simplejson/encoder.py
+++ simplejson/encoder.py
@@ -13,7 +13,7 @@ def _import_speedups():
         return None, None
 c_encode_basestring_ascii, c_make_encoder = _import_speedups()
 
-from simplejson.decoder import PosInf
+from .decoder import PosInf
 
 #ESCAPE = re.compile(ur'[\x00-\x1f\\"\b\f\n\r\t\u2028\u2029]')
 # This is required because u() will mangle the string and ur'' isn't valid

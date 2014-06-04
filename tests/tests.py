ce3935bce6e67b1f0b15c16fa65639302b39bb89
Nikolajus Krauklis <nikolajus@gmail.com>
2014-06-04 17:03:58 +0200

    pep8 small fixes and fixing jQ to work on SublimeText 2. Definition of show_input_panel was wrong

--- tests/tests.py
+++ tests/tests.py
@@ -12,7 +12,6 @@ import unittest
 
 
 class TestIssues(unittest.TestCase):
-
     def setUp(self):
         pass
 
@@ -23,8 +22,8 @@ class TestIssues(unittest.TestCase):
 }'''
         obj = json.loads(tmp_str, object_pairs_hook=OrderedDict, parse_float=decimal.Decimal)
         tmp_str = json.dumps(obj, indent=2, ensure_ascii=True, sort_keys=False,
-                    separators=(',', ': '),
-                    use_decimal=True)
+                             separators=(',', ': '),
+                             use_decimal=True)
         self.assertEqual(tmp_str, expected_output)
 
     # issue 15
@@ -35,8 +34,8 @@ class TestIssues(unittest.TestCase):
 }"""
         obj = json.loads(tmp_str, object_pairs_hook=OrderedDict, parse_float=decimal.Decimal)
         tmp_str = json.dumps(obj, indent=2, ensure_ascii=False, sort_keys=False,
-                    separators=(',', ': '),
-                    use_decimal=True)
+                             separators=(',', ': '),
+                             use_decimal=True)
         self.assertEqual(tmp_str, expected_output)
 
     # issue 16
@@ -45,13 +44,14 @@ class TestIssues(unittest.TestCase):
         obj = json.loads(tmp_str, object_pairs_hook=OrderedDict, parse_float=decimal.Decimal)
         self.assertEqual(obj['float'], 1.0)
         tmp_str = json.dumps(obj, indent=0, ensure_ascii=False, sort_keys=False,
-                    separators=(',', ': '),
-                    use_decimal=True)
+                             separators=(',', ': '),
+                             use_decimal=True)
         self.assertEqual(tmp_str.split('\n')[1], '"float": 1.0')
 
     # issue 16
     def test_float_issue_16_2(self):
-        tmp_str = '{"test1":0.99, "test2":"1.99", "test3":1.00000000001, "test4":1.99, "test5":1, "test6":4.589999999999999999, "test7":1.0}'
+        tmp_str = '{"test1":0.99, "test2":"1.99", "test3":1.00000000001, "test4":1.99, "test5":1,' \
+                  ' "test6":4.589999999999999999, "test7":1.0}'
         expected_output = """{
   "test1": 0.99,
   "test2": "1.99",
@@ -63,8 +63,8 @@ class TestIssues(unittest.TestCase):
 }"""
         obj = json.loads(tmp_str, object_pairs_hook=OrderedDict, parse_float=decimal.Decimal)
         tmp_str = json.dumps(obj, indent=2, ensure_ascii=False, sort_keys=False,
-                    separators=(',', ': '),
-                    use_decimal=True)
+                             separators=(',', ': '),
+                             use_decimal=True)
         self.assertEqual(tmp_str, expected_output)
 
     def test_compress_feature(self):
@@ -74,10 +74,9 @@ class TestIssues(unittest.TestCase):
         expected_output = '{"real":0.99}'
         obj = json.loads(tmp_str, object_pairs_hook=OrderedDict, parse_float=decimal.Decimal)
         tmp_str = json.dumps(obj, ensure_ascii=False, sort_keys=False,
-                    separators=(',', ':'),
-                    use_decimal=True)
+                             separators=(',', ':'),
+                             use_decimal=True)
         self.assertEqual(tmp_str, expected_output)
-
 
 
 if __name__ == '__main__':

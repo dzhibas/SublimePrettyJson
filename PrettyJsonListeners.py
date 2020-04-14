46cfff357ebfa224cabe6f2858a5e1e5183faf2b
TheSecEng <zachary.schulze@gmail.com>
2020-04-14 14:09:35 -0400

    fix: formatting

--- PrettyJsonListeners.py
+++ PrettyJsonListeners.py
@@ -14,11 +14,11 @@ class PrettyJsonLintListener(sublime_plugin.EventListener, PrettyJsonBaseCommand
     def on_post_save(self, view):
         # will work only in json syntax and once validate_on_save setting is true
         validate = s.get("validate_on_save", True)
-        if validate and "JSON" in view.settings().get('syntax'):
+        if validate and "JSON" in view.settings().get("syntax"):
             self.view = view
 
-            self.view.erase_regions('json_errors')
-            self.view.erase_status('json_errors')
+            self.view.erase_regions("json_errors")
+            self.view.erase_status("json_errors")
 
             json_content = self.view.substr(sublime.Region(0, view.size()))
 
@@ -31,5 +31,5 @@ class PrettyJsonLintListener(sublime_plugin.EventListener, PrettyJsonBaseCommand
 class PrettyJsonAutoPrettyOnSaveListener(sublime_plugin.EventListener):
     def on_pre_save(self, view):
         auto_pretty = s.get("pretty_on_save", False)
-        if auto_pretty and "JSON" in view.settings().get('syntax'):
-            sublime.active_window().run_command('pretty_json')
+        if auto_pretty and "JSON" in view.settings().get("syntax"):
+            sublime.active_window().run_command("pretty_json")

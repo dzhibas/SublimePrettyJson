37464164b3951cc31b12bb5043d89b24e545f0e7
deathaxe <deathaxe82@googlemail.com>
2024-07-31 15:03:47 +0200

    fix PrettyJsonLintListener
    This commit converts `PrettyJsonLintListener` into a `ViewEventListener`,
    so it contains `self.view` member, required by mixed in `PrettyJsonBaseCommand`
    class.

--- PrettyJsonListeners.py
+++ PrettyJsonListeners.py
@@ -6,15 +6,16 @@ from .PrettyJson import PrettyJsonBaseCommand
 s = sublime.load_settings("Pretty JSON.sublime-settings")
 
 
-class PrettyJsonLintListener(sublime_plugin.EventListener, PrettyJsonBaseCommand):
-    def on_post_save(self, view):
+class PrettyJsonLintListener(sublime_plugin.ViewEventListener, PrettyJsonBaseCommand):
+    def on_post_save(self):
         if not s.get("validate_on_save", True):
             return
 
         as_json = s.get("as_json", ["JSON"])
-        if any(syntax in view.settings().get("syntax") for syntax in as_json):
+        view_syntax = self.view.settings().get("syntax")
+        if any(syntax in view_syntax for syntax in as_json):
             self.clear_phantoms()
-            json_content = view.substr(sublime.Region(0, view.size()))
+            json_content = self.view.substr(sublime.Region(0, self.view.size()))
             try:
                 self.json_loads(json_content)
             except Exception as ex:
